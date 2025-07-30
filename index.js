5847e9c97aba4237f175b3ee6e5f8d200cbada4e
Tanay-77 <tanaymahajan7@gmail.com>
2025-07-30 20:27:37 +0530

    index file is changed

---
+++ index.js
@@ -0,0 +1,16 @@
+// git config -- user.name 'tanay-77'
+// git config -- user.email 'tanay-77'
+// git config --global init.defaultBranch main
+// git init 
+// type nul > file name
+//  md tanay to create a folder
+// git status
+// git add index.html  or git add .
+// git commit -m "index file is changed"
+// git log to check commit
+// git reset 49034039 id of commit yu want
+// git remote add origin  url of repo
+// got remote -v to get list of repo
+// git push origin main
+// git branch feature // add feature as new branch 
+// git checkout feature  // shift us to feature branch 
